140e526bb96aae39e9a3d6dd9c2ad9b3ae79b6b7
RobertJunghanns <51117160+RobertJunghanns@users.noreply.github.com>
2020-12-18 15:16:23 +0100

    Update commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-empty': [2, 'never'],
-    'scope-case': [2, 'always', ['pascal-case', 'camel-case', 'lower-case']],
-    'header-max-length': [0, 'always', 96]
   }
 };
